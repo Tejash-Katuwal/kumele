ba577773bcaa83daa6889d8cbac8e5c1fc1ae04f
Tejash-Katuwal <tejashkatuwal99@gmail.com>
2025-04-06 22:56:25 +0545

    cloudinary icons

--- hobbies/views.py
+++ hobbies/views.py
@@ -7,11 +7,19 @@ from .models import Hobby
 from .serializers import HobbySerializer, SelectHobbiesSerializer
 
 class ListHobbiesView(APIView):
-    permission_classes = [AllowAny]  
+    permission_classes = [AllowAny]
+    
     def get(self, request):
         hobbies = Hobby.objects.all()
-        serializer = HobbySerializer(hobbies, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        # Custom serialization to prioritize icon_url
+        hobby_data = []
+        for hobby in hobbies:
+            hobby_data.append({
+                'id': hobby.id,
+                'name': hobby.name,
+                'icon_url': hobby.icon_url or ''
+            })
+        return Response(hobby_data, status=status.HTTP_200_OK)
     
 class SelectHobbiesView(APIView):
     permission_classes = [IsAuthenticated]
@@ -21,9 +29,19 @@ class SelectHobbiesView(APIView):
         serializer = SelectHobbiesSerializer(user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
+            
+            # Custom serialization for hobbies to ensure we use icon_url
+            hobbies_data = []
+            for hobby in user.hobbies.all():
+                hobbies_data.append({
+                    'id': hobby.id,
+                    'name': hobby.name,
+                    'icon_url': hobby.icon_url or ''
+                })
+                
             return Response({
                 "message": "Hobbies updated successfully",
-                "hobbies": HobbySerializer(user.hobbies.all(), many=True).data,
+                "hobbies": hobbies_data,
                 "next_step": "welcome"  # After hobbies, direct to welcome screen
             }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
