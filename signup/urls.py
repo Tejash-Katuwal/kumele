64479df9430b139ec4ba0df31b037f1baf36eee5
Tejash-Katuwal <tejashkatuwal99@gmail.com>
2025-04-07 23:00:31 +0545

    april 7 commit

--- signup/urls.py
+++ signup/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
-from .views import SignupView, VerifyEmailView, GoogleSignInView, DeleteUserAPIView, UpdatePermissionsView, SetUsernameView, LoginView
+from .views import (SignupView,
+    VerifyEmailView, 
+    GoogleSignInView, 
+    DeleteUserAPIView, 
+    UpdatePermissionsView, 
+    SetUsernameView, 
+    LoginView, 
+    UpdateUserDetailsView,
+    PasskeyRegistrationOptionsView,
+    PasskeyRegistrationVerifyView,
+    PasskeyLoginOptionsView,
+    PasskeyLoginVerifyView,
+)
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
@@ -9,4 +21,10 @@ urlpatterns = [
     path('update-permissions/', UpdatePermissionsView.as_view(), name='update-permissions'),
     path('set-username/', SetUsernameView.as_view(), name='set-username'),
     path('login/', LoginView.as_view(), name='login'),
+    path('update-user-details/', UpdateUserDetailsView.as_view(), name='update-user-details'),
+    path('passkey/register/options/', PasskeyRegistrationOptionsView.as_view(), name='passkey_register_options'),
+    path('passkey/register/verify/', PasskeyRegistrationVerifyView.as_view(), name='passkey_register_verify'),
+    path('passkey/login/options/', PasskeyLoginOptionsView.as_view(), name='passkey_login_options'),
+    path('passkey/login/verify/', PasskeyLoginVerifyView.as_view(), name='passkey_login_verify'),
+
 ]
